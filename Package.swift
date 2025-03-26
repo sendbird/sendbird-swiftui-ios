1b56774141c9e5e9e729abaaa0b12388075432eb
Celine Moon <peaceluverceline@gmail.com>
2025-03-26 16:45:28 +0900

    testing package dependency.

--- Package.swift
+++ Package.swift
@@ -19,6 +19,10 @@ let package = Package(
             url: "https://github.com/sendbird/sendbird-chat-sdk-ios",
             from: "4.25.0"
         ),
+        .package(
+            url: "https://github.com/sendbird/sendbird-uikit-ios",
+            from: "3.30.1"
+        ),
     ],
     targets: [
         .binaryTarget(
@@ -27,16 +31,11 @@ let package = Package(
             checksum: "6c9bf21d3fd6faf59d24b14b1bf5f5c81cb20ef01fb3bd8e23bcacfba26ec803"
             
         ),
-        .binaryTarget(
-            name: "SendbirdUIMessageTemplate",
-            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/3.30.1/SendbirdUIMessageTemplate.xcframework.zip" // SendbirdUIMessageTemplate_URL
-            checksum: "af0563350430be0ecb40d20d0365bf7f5210719055b4d274570a4a62cbcb2e8c" // SendbirdUIMessageTemplate_CHECKSUM
-        ),
         .target(
             name: "SendbirdSwiftUITarget",
             dependencies: [
                 .target(name: "SendbirdSwiftUI"),
-                .target(name: "SendbirdUIMessageTemplate"),
+                .product(name: "SendbirdUIMessageTemplate", package: "sendbird-uikit-ios"),
                 .product(name: "SendbirdChatSDK", package: "SendbirdChatSDK")
             ],
             path: "Framework/Dependency",
