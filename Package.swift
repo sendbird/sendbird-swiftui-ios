8d67c032fde2d09db272ee622b2c795f09c0ffa3
Celine Moon <peaceluverceline@gmail.com>
2025-10-31 10:36:06 +0900

    Removed internal dependency on `SendbirdUIMessageTemplate` target, modified it as external package dependency.

--- Package.swift
+++ Package.swift
@@ -15,9 +15,12 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            name: "SendbirdChatSDK",
             url: "https://github.com/sendbird/sendbird-chat-sdk-ios",
-            from: "4.29.0"
+            from: "4.32.0"
+        ),
+        .package(
+            url: "https://github.com/sendbird/sendbird-uikit-ios-spm",
+            from: "3.32.3"
         ),
     ],
     targets: [
@@ -25,19 +28,13 @@ let package = Package(
             name: "SendbirdSwiftUI",
             url: "https://github.com/sendbird/sendbird-swiftui-ios/releases/download/1.1.1/SendbirdSwiftUI.xcframework.zip", // SendbirdSwiftUI_URL
             checksum: "cc2815f523db320c30092e97a48bf0575496742f5a074130d5bf4dbc7f4e4d80" // SendbirdSwiftUI_CHECKSUM
-            
-        ),
-        .binaryTarget(
-            name: "SendbirdUIMessageTemplate",
-            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/3.32.2/SendbirdUIMessageTemplate.xcframework.zip", // SendbirdUIMessageTemplate_URL
-            checksum: "380b1948377c740febd0b324e21b181e2fc3e3717eaf08886eb3d9196c0b9602" // SendbirdUIMessageTemplate_CHECKSUM
         ),
         .target(
             name: "SendbirdSwiftUITarget",
             dependencies: [
                 .target(name: "SendbirdSwiftUI"),
-                .target(name: "SendbirdUIMessageTemplate"),
-                .product(name: "SendbirdChatSDK", package: "SendbirdChatSDK")
+                .product(name: "SendbirdChatSDK", package: "SendbirdChatSDK"),
+                .product(name: "SendbirdUIMessageTemplate", package: "sendbird-uikit-ios-spm"),
             ],
             path: "Framework/Dependency",
             exclude: ["../../Sample", "../../Sources"]
